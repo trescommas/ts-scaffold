7f64f0faf3882b344d5ff4ee1b7c89510fad4e3c
Abhisheak Mall <shakecross@gmail.com>
2021-10-21 13:49:04 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function main() {
+  console.log("working!");
+}
+
+main();
---
+++ test/example.test.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("test", () => {
+  it("example test", async () => {
+    expect(true).eq(true);
+  });
+});
